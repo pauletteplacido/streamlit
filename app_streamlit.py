5719cd2e2ca6caa70ef56ad2e6d6da36b6cd8702
Paulette Placido <pauletteplacido@gmail.com>
2024-04-25 18:08:27 -0300

    commit

--- app_streamlit.py
+++ app_streamlit.py
@@ -3,15 +3,18 @@ import time
 
 
 def load_dashboard():
-    url_dashboard = "https://lookerstudio.google.com/embed/reporting/58146385-eed4-4f54-82cd-c11d81075572/page/WGxxD"
+    url_dashboard = "https://lookerstudio.google.com/embed/reporting/58146385eed44f5482cd-c11d81075572/page/WGxxD"
+    iframe_code = (
+        '<iframe src="' + url_dashboard + '" '
+        'width="100%" height="600" frameborder="0">'
+        '</iframe>'
+    )
     st.markdown(
-        f'<div style="background-color: #333333; padding: 20px; border-radius: 5px;">'
-        f'<iframe src="{
-            url_dashboard}" width="100%" height="600" frameborder="0"></iframe>'
-        f'</div>',
+        '<div style="background-color: #333333; padding: 20px; border-radius: 5px;">'
+        + iframe_code +
+        '</div>',
         unsafe_allow_html=True
     )
-
 
 def main():
     # Configura la página de Streamlit
